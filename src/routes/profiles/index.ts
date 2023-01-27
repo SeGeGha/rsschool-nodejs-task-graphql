136cf05d79dcfd69416e41db38461a9fd5e602ba
Sergey Sudakov <sergey.sudakov@ontravelsolutions.com>
2023-01-27 16:12:25 +0300

    feat: implement profiles route endpoints

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { idParamSchema } from '../../utils/reusedSchemas';
+import { validateId } from '../../utils/uuidValidator';
 import { createProfileBodySchema, changeProfileBodySchema } from './schema';
 import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
 
@@ -8,7 +9,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     ProfileEntity[]
-  > {});
+  > {
+    return this.db.profiles.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +20,16 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params as Record<'id', string>;
+
+      const profile = await this.db.profiles.findOne({ key: 'id', equals: id });
+      if (!profile) {
+        throw reply.notFound(`Profile with id ${id} not found`);
+      }
+
+      return profile;
+    }
   );
 
   fastify.post(
@@ -27,7 +39,19 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createProfileBodySchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { memberTypeId, userId } = request.body as ProfileEntity;
+      const memberType = await this.db.memberTypes.findOne({ key: 'id', equals: memberTypeId });
+      if (!memberType) {
+        throw reply.badRequest(`Invalid member type id ${memberTypeId}`);
+      }
+      const userProfile = await this.db.profiles.findOne({ key: 'userId', equals: userId });
+      if (userProfile) {
+        throw reply.badRequest(`User has profile`);
+      }
+
+      return this.db.profiles.create(request.body as Omit<ProfileEntity, 'id' | 'subscribedToUserIds'>);
+    }
   );
 
   fastify.delete(
@@ -37,7 +61,18 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params as Record<'id', string>;
+      if (!validateId(id)) {
+        throw reply.badRequest(`Invalid profile id ${id}`);
+      }
+
+      try {
+        return await this.db.profiles.delete(id);
+      } catch (error) {
+        throw reply.notFound(`Profile with id ${id} not found`);
+      }
+    }
   );
 
   fastify.patch(
@@ -48,7 +83,18 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity> {
+      const { id } = request.params as Record<'id', string>;
+      if (!validateId(id)) {
+        throw reply.badRequest(`Invalid profile id ${id}`);
+      }
+
+      try {
+        return await this.db.profiles.change(id, request.body as Partial<Omit<ProfileEntity, 'id' | 'userId'>>);
+      } catch (error) {
+        throw reply.notFound(`Profile with id ${id} not found`);
+      }
+    }
   );
 };
 
